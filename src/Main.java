2740f3327e9ea29a0daa60ddd366fd467e83f835
André <andrev.adm@gmail.com>
2018-09-19 21:24:36 -0300

    Conectando banco SQLite.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,38 @@
+import java.sql.*;
+import java.io.Console;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        // Conexão Firebird
+        Connection connection = null;
+        Statement statement = null;
+        try {
+            String url = "jdbc:sqlite:C:/projetosUFMG/clinicaMedica/src/banco/clinicaMedica.db";
+            connection = DriverManager.getConnection(url);
+
+            System.out.println("Connection to SQLite has been established.");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        Console console = System.console();
+        Scanner entrada = new Scanner(System.in);
+        String login, senhaCorreta = "123", senha;
+
+        System.out.println("Master Clínica seu programa de clínicas médicas!\n");
+        System.out.print("Login: ");
+        login = entrada.next();
+
+        System.out.print("Senha: ");
+        senha = entrada.next();
+
+        if (senha.compareTo(senhaCorreta) == 0){
+            System.out.println("\nBem vindo " + login + "!");
+        }else {
+            System.out.println("\nSenha incorreta!");
+        }
     }
 }
